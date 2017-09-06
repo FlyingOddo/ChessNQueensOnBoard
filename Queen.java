3bfc28c529af4a16df1862b7aae6bceb065f4f2d
FlyingOddo <31707241+FlyingOddo@users.noreply.github.com>
2017-09-06 22:19:23 +0200

    Add files via upload
    Add the class with the logic

---
+++ Queen.java
@@ -0,0 +1,90 @@
+public class Queen {
+ 
+	int[] chessRow;
+	int numTries = 0;
+	boolean finished = false;
+	boolean firstKnightRound = false;
+
+	public Queen(int N) { 
+		chessRow = new int[N];
+	}
+	
+	public static void main(String args[]) {
+		Queen Q = new Queen(10);
+		Q.callplaceQueens();
+	}
+	
+	public void callplaceQueens() {
+		placeQueens(0, chessRow.length);
+	}
+	
+	public void placeQueens(int r, int n) {
+		numTries++;
+
+		/* set the first row to the secound field */
+		if (n % 2 == 0) {
+			chessRow[0] = 1;
+		}
+
+		for (int c = 0; c < n; c++) {
+
+			if (r >= 1) {
+				if (chessRow[r - 1] + 2 <= n && firstKnightRound == false) {
+					c = chessRow[r - 1] + 2;
+				} else if (chessRow[r - 1] + 2 > n) {
+					firstKnightRound = true;
+
+				}
+			}
+
+			if (checkPositionQueen(r, c)) {
+				chessRow[r] = c;
+				/*
+				 * skip the next column bc thats not possible to set the queen
+				 * there
+				 */
+				c++;
+
+				if (r == n - 1) {
+					printResult(chessRow);
+					return;
+				} else {
+					if (finished == false) {
+						placeQueens(r + 1, n);
+					}
+				}
+			}
+		}
+	}
+
+	public boolean checkPositionQueen(int row, int column) {
+
+		for (int i = 0; i < row; i++) {
+			if (chessRow[i] == column || (i - row) == (chessRow[i] - column) || (i - row) == (column - chessRow[i])) {
+				return false;
+			}
+		}
+		return true;
+	} 
+
+	public void printResult(int[] queens) {
+		int N = queens.length;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (queens[i] == j) {
+					System.out.print("O ");
+
+				} else {
+					System.out.print("X ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println(); 
+
+		finished = true;
+
+	}
+
+}
